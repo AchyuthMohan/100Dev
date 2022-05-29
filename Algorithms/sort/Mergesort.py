c17dc42ea1e19e58d48ddd98c0435350f34e7152
annu12340 <annujolly17@gmail.com>
2022-05-29 13:29:36 +0530

    code refactor

--- Algorithms/sort/Mergesort.py
+++ Algorithms/sort/Mergesort.py
@@ -1,11 +1,14 @@
-# Python program for implementation of MergeSort
-
-# Merges two subarrays of arr[].
-# First subarray is arr[l..m]
-# Second subarray is arr[m+1..r]
-
-
 def merge(arr, l, m, r):
+    """
+    Python program for implementation of MergeSort
+    Merges two subarrays of arr[].
+
+    arr:
+    First subarray is arr[l..m]
+
+    arr:
+    Second subarray is arr[m+1..r]
+    """
     n1 = m - l + 1
     n2 = r - m
 
--- stackoverflow/readme.md
+++
@@ -1,8 +0,0 @@
-## Usage
-
-- Mention file name in stackoverflow.py, line 27
-
-## Others
-
-- In line 10, tagged=python: update this to update all languages depending on the file extensions
-eg, abc.js -> then tagged=javascript
